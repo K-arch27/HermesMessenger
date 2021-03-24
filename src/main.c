b1cefeb46e8d49c24c15bad0e3c109ba359d4b10
MrBn100ful <mrbn100ful@neonroot.net>
2021-03-24 17:32:11 +0100

    some more work

--- src/main.c
+++ src/main.c
@@ -20,6 +20,9 @@
 #include <glib-object.h>
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
+#include <JavaScriptCore/JSStringRef.h>
+#include <JavaScriptCore/JSValueRef.h>
+
 
 static void destroyWinCb(GtkWidget* widget, GtkWidget* window)
 {
@@ -29,71 +32,26 @@ static void destroyWinCb(GtkWidget* widget, GtkWidget* window)
 static gboolean closeWebCb(WebKitWebView* webView, GtkWidget* window)
 {
     gtk_widget_destroy(window);
+
     return TRUE;
 }
-
-
-static void
-web_view_javascript_finished (GObject      *object,
-                              GAsyncResult *result,
-                              gpointer      user_data)
-{
-    WebKitJavascriptResult *js_result;
-    JSValueRef              value;
-    JSGlobalContextRef      context;
-    GError                 *error = NULL;
-
-    js_result = webkit_web_view_run_javascript_finish (WEBKIT_WEB_VIEW (object), result, &error);
-    if (!js_result) {
-        g_warning ("Error running javascript: %s", error->message);
-        g_error_free (error);
-        return;
-    }
-
-    context = webkit_javascript_result_get_global_context (js_result);
-    value = webkit_javascript_result_get_value (js_result);
-    if (JSValueIsString (context, value)) {
-        JSStringRef js_str_value;
-        gchar      *str_value;
-        gsize       str_length;
-
-        js_str_value = JSValueToStringCopy (context, value, NULL);
-        str_length = JSStringGetMaximumUTF8CStringSize (js_str_value);
-        str_value = (gchar *)g_malloc (str_length);
-        JSStringGetUTF8CString (js_str_value, str_value, str_length);
-        JSStringRelease (js_str_value);
-        g_print ("Script result: %s\n", str_value);
-        g_free (str_value);
-    } else {
-        g_warning ("Error running javascript: unexpected return value");
-    }
-    webkit_javascript_result_unref (js_result);
-}
-
-static void
-web_view_get_link_url (WebKitWebView *web_view,
-                       const gchar   *link_id)
-{
-    gchar *script;
-
-    script = g_strdup_printf ("alert('yes!');", link_id);
-    webkit_web_view_run_javascript (web_view, script, NULL, web_view_javascript_finished, NULL);
-    g_free (script);
-}
-
 
 int main(int argc, char* argv[])
 {
     GtkWidget *win;
+
     WebKitWebView *web;
+
+    gchar *script;
+
     gchar * url = "https://messenger.com/login";
 
     gtk_init(&argc, &argv);
 
-    if (argc == 2)
-        url = argv[1];
+    if (argc == 2) url = argv[1];
 
     win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
     gtk_window_set_default_size(GTK_WINDOW(win), 360, 720);
 
     web = WEBKIT_WEB_VIEW(webkit_web_view_new());
@@ -101,17 +59,22 @@ int main(int argc, char* argv[])
     gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(web));
 
     g_signal_connect(win, "destroy", G_CALLBACK(destroyWinCb), NULL);
+
     g_signal_connect(web, "close", G_CALLBACK(closeWebCb), win);
 
     webkit_web_view_load_uri(web, url);
-
-    web_view_get_link_url(web,url);
 
     gtk_widget_grab_focus(GTK_WIDGET(web));
 
     gtk_widget_show_all(win);
 
     gtk_main();
+
+    script = g_strdup_printf ("alert('yes!');", url);
+
+    webkit_web_view_run_javascript (web, script, NULL, NULL, NULL);
+
+    g_free (script);
 
 
 
